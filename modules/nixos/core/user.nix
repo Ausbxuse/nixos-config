003aaf64baa67714cc53c2cd0cd85448c6620a40
Zhenyu Zhao <peterzhaozzy@gmail.com>
2026-04-01 17:13:50 -0700

    fix syntax + slimevr

--- modules/nixos/core/user.nix
+++ modules/nixos/core/user.nix
@@ -14,11 +14,14 @@
   users.defaultUserShell = pkgs.zsh;
   users.users.${const.username} = {
     isNormalUser = true;
-    extraGroups = ["wheel" "networkmanager" "video"];
+    extraGroups = ["wheel" "networkmanager" "video" "dialout"];
     home = "/home/${const.username}";
     initialHashedPassword = "";
     createHome = true;
   };
+  services.udev.extraRules = ''
+    SUBSYSTEM=="hidraw", ATTRS{idVendor}=="1209", ATTRS{idProduct} =="7690", MODE="0660", GROUP="dialout", TAG+="uaccess"
+  '';
 
   security.sudo = {
     enable = true;
